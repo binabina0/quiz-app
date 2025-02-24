1077264a62ea52580c41faaf5067458a4a2a6af9
binabina0 <malbinca.1111@gmail.com>
2025-02-24 15:09:11 +0600

    fixing Question repository

--- src/main/java/com/example/quizapp/enteties/Question.java
+++ src/main/java/com/example/quizapp/enteties/Question.java
@@ -1,23 +1,25 @@
 package com.example.quizapp.enteties;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
 
-@Data
+import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
+@Data
+//@Table(name = "question")
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
+//    @Column(name = "question_title")
     private String questionTitle;
     private String option1;
     private String option2;
     private String option3;
     private String option4;
+//    @Column(name = "right_answer")
     private String rightAnswer;
+//    @Column(name = "difficulty_level")
     private String difficultyLevel;
     private String category;
 }
