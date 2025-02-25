bc201b26d95a2b342395abefbae73bd30800e388
binabina0 <malbinca.1111@gmail.com>
2025-02-25 17:05:53 +0600

    ResponseEnteties for allQuestions, byCategory, add with exception

--- src/main/java/com/example/quizapp/controllers/QuestionController.java
+++ src/main/java/com/example/quizapp/controllers/QuestionController.java
@@ -2,6 +2,8 @@ package com.example.quizapp.controllers;
 
 import com.example.quizapp.enteties.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.quizapp.services.QuestionService;
 
@@ -14,17 +16,17 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions() {
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 }
