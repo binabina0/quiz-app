3b1bbf63a38b74b2cceb34b4baea2bda491dbe32
binabina0 <malbinca.1111@gmail.com>
2025-02-26 16:07:29 +0600

    create quiz; Quiz, QuizController, QuizService, QuizDao, update QuestionDao

--- src/main/java/com/example/quizapp/dao/QuestionDao.java
+++ src/main/java/com/example/quizapp/dao/QuestionDao.java
@@ -1,6 +1,7 @@
 package com.example.quizapp.dao;
 import com.example.quizapp.enteties.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,4 +9,7 @@ import java.util.List;
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
     List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionByCategory(String category, int numQ);
 }
