efa19304a3ed7e063d56ab8482f59d6958e4463c
binabina0 <malbinca.1111@gmail.com>
2025-05-01 00:37:56 +0600

    add login

--- src/main/java/com/example/quizapp/controllers/AuthController.java
+++ src/main/java/com/example/quizapp/controllers/AuthController.java
@@ -1,9 +1,17 @@
 package com.example.quizapp.controllers;
 
+
+import com.example.quizapp.jwt.JwtResponse;
+import com.example.quizapp.jwt.RefreshTokenService;
+import com.example.quizapp.payload.LoginRequest;
 import com.example.quizapp.payload.RegisterRequest;
 import com.example.quizapp.services.AuthService;
+import com.example.quizapp.jwt.JwtService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     AuthService authService;
+    AuthenticationManager authenticationManager;
+    RefreshTokenService refreshTokenService;
+    JwtService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
         return authService.register(registerRequest);
     }
 
-//    @PostMapping("/login")
-//    public ResponseEntity<String> authenticate(String username, String password) {}
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> authenticate(@RequestBody LoginRequest loginRequest)  {
+        return authService.login(loginRequest);
+    }
 }
--- src/main/java/com/example/quizapp/services/AuthService.java
+++ src/main/java/com/example/quizapp/services/AuthService.java
@@ -1,18 +1,33 @@
 package com.example.quizapp.services;
 
+import com.example.quizapp.config.UserDetailsDao;
 import com.example.quizapp.dao.UserDao;
+import com.example.quizapp.jwt.JwtResponse;
+import com.example.quizapp.enteties.Role;
 import com.example.quizapp.enteties.User;
+import com.example.quizapp.jwt.JwtService;
+import com.example.quizapp.payload.LoginRequest;
 import com.example.quizapp.payload.RegisterRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
-    @Autowired
-    UserDao userDao;
+    private final UserDao userDao;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public ResponseEntity<String> register(RegisterRequest registerRequest) {
         if(userDao.findByUsername(registerRequest.getUsername()).isPresent()) {
             return new ResponseEntity<>("Username is already in use", HttpStatus.BAD_REQUEST);
@@ -22,10 +37,36 @@ public class AuthService {
         }
         User user = new User();
         user.setUsername(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
+        user.setRoles(List.of(Role.USER));
         user.setEnabled(true);
         userDao.save(user);
+
         return new ResponseEntity<>("User successfully registered", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<JwtResponse> login(LoginRequest loginRequest) {
+        try{
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            UserDetailsDao userDetails = (UserDetailsDao)  authentication.getPrincipal();
+            String jwt = jwtService.generateToken(userDetails);
+            JwtResponse jwtResponse = new JwtResponse();
+            jwtResponse.setToken(jwt);
+            jwtResponse.setExpiresIn(jwtService.getExpirationTime());
+            return new ResponseEntity<>(jwtResponse, HttpStatus.OK);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
     }
 }
